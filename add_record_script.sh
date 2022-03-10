17ec37cdbdbafd08629a05b25c11fe6e36f66418
Applegammer <93336699+Applegammer@users.noreply.github.com>
2022-03-10 12:25:16 +0100

    Update script
    Add code

--- add_record_script.sh
+++ add_record_script.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+# declare STRING variable
+STRING="Add new record to DB - test"
+# print variable on a screen
+echo $STRING
+sudo -u postgres psql -c "INSERT INTO testtable VALUES ('my-site-$RANDOM.com');"
